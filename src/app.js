ca96afd41e76ff9e03982828748371c035e6786c
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-01-07 21:49:01 +0530

    Initialize database

--- src/app.js
+++ src/app.js
@@ -1,29 +1,34 @@
 const express = require('express');
-
 const app = express();
-const {adminAuth ,userAuth} = require("./middlewares/auth")
-app.use('/',(err,req,res,next)=>{  //this will run only erro present but till here error is not present
-    if(err){
-        res.status(500).send("something went wrong");
+const connectDB = require("./config/database");
+const User = require("./models/User")
+
+
+
+app.post('/signup', async (req,res)=>{
+    // creating a new instance of the User model
+    const userObj = {
+        firstName:"Sachin",
+        lastName:"tendulkar",
+        email : "sachin@mail.com",
+        password : "sachin@123",
     }
-})
-app.get('/getUserData', (req,res)=>{
-    //logic of DB call and get user data
-    try{
-        throw new Error(" random error");
-        res.send("user data send");
+    const user = new User(userObj);
+
+    try {
+        await user.save();
+        res.send("User added successfully")
+        
+    } catch (error) {
+        res.status(400).send("Error saving the user in the database"+error.message)
     }
-    catch(err){
-        console.log("error in catch"+err);
-        res.status(500).send("some error occured contact support.")
-    }
-})
-// err is the first parameter.
-app.use('/',(err,req,res,next)=>{
-    if(err){
-        res.status(500).send("something went wrong");
-    }
-})
-app.listen(8080,()=>{
-    console.log("server is successfully listening on port 8080");   
+});
+
+connectDB().then(()=>{
+    console.log("database connection established.");
+    app.listen(8080,()=>{
+        console.log("server is successfully listening on port 8080");   
+    })
+}).catch((err)=>{
+    console.log("database cannot be connected!");
 })
