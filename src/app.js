6ee4afee64d4ce00edba1bcfd4b24b524b9a2d8a
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-01-07 22:53:17 +0530

    create /signup /feed api

--- src/app.js
+++ src/app.js
@@ -3,27 +3,41 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/User")
 
-
+// we use the middleware express.json() to conver the req.data to all the routes that why we do not use any route
+app.use(express.json());
 
 app.post('/signup', async (req,res)=>{
-    // creating a new instance of the User model
-    const userObj = {
-        firstName:"Sachin",
-        lastName:"tendulkar",
-        email : "sachin@mail.com",
-        password : "sachin@123",
-    }
-    const user = new User(userObj);
-
+    const user = new User(req.body);
     try {
-        await user.save();
-        res.send("User added successfully")
-        
+        // console.log(req.body)
+        await user.save();        
+        res.send("User added successfully!");
     } catch (error) {
-        res.status(400).send("Error saving the user in the database"+error.message)
+        res.status(400).send("Error saving the user"+error.message)
     }
 });
 
+// feet api - GET /feed - get all the users from the database.
+app.get('/feed', async (req,res)=>{
+    const userName = req.body.firstName;
+    try {
+        // const users = await User.find() //give any random document
+        // const users = await User.find({})//find all the users from the collection
+
+        const users = await User.find({firstName:userName})
+        if(users.length === 0){
+            res.status(404).send("user not found");
+        }
+        else{
+            res.send(users);
+        }
+
+        
+    } catch (error) {
+        res.status(400).send("something went wrong");
+    }
+})
+ 
 connectDB().then(()=>{
     console.log("database connection established.");
     app.listen(8080,()=>{
