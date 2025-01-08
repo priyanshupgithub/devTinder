33f21d8198e9a6f025da99341a1bc3fc2cc156b4
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-01-08 18:41:50 +0530

    Exploring validator library

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const validator = require('validator');
 const userSchema = mongoose.Schema({
     firstName:{
         type:String,
@@ -16,11 +16,21 @@ const userSchema = mongoose.Schema({
         unique:true,
         lowercase:true,
         trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is not valid!"+value);
+            }
+        }
 
     },
     password:{
         type:String,
         required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Please enter a strong password!"+value);
+            }
+        }
     },
     age:{
         type:Number,
@@ -37,7 +47,12 @@ const userSchema = mongoose.Schema({
     },
     photoUrl:{
         type:String,
-        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWRrWgjZtjvfCpKF-uof_08e89WR9269oYsA&s"
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWRrWgjZtjvfCpKF-uof_08e89WR9269oYsA&s",
+        validate(value){   //here we added a custom validation function and this validator functions runs onyl on creating new user and does not run/validate when patch(existing data) so for run this we have to add it in the patch api in (options)
+            if(!validator.isURL(value)){
+                throw new Error("Invalid Photo Url"+value);
+            }
+        }
     },
     about:{
         type:String,
