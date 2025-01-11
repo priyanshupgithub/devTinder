87434edf4f87092d2ca659ea7aa4bc0c8ed3ccbb
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-01-12 02:33:43 +0530

    Building Feed Api and Pagination

--- src/apiList.md
+++ src/apiList.md
@@ -21,4 +21,13 @@ userRouter
 -GET /user/requests/recieved
 -GET /user/feed - gets you the profile of other users on platform
 
+/feed?page=1&limit=10 => 1-10  .skip(0) & limit(10)
+/feed?page=2&limit=10 => 11-20 .skip(10) & limit(10)
+/feed?page=3&limit=10 => 21-30 .skip(20) & limit(10)
+
+two imp functions
+.skip(0)   -> means skips 0 
+.limit(10) -> means set limit to 10
+so formula of skip can be :  (page-1)*limit
+
 status : ignore,interested,accepted,rejected
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,12 @@ const { userAuth } = require("../middlewares/auth");
 const { Connection } = require("mongoose");
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require('../models/User');
 
 
 
 const USER_SAFE_DATA = ["firstName","lastName","age","gender","photoUrl","skills","about"];
+
 userRouter.get("/user/request/received", userAuth , async (req,res)=>{  //get user request means all the request which are pending for (accepted/rejected)
     try {
         const loggedInUser = req.user;
@@ -49,8 +51,62 @@ userRouter.get("/user/connections", userAuth, async (req,res)=>{
 
         res.json({data})
     } catch (error) {
-        
+        res.status(400).send("ERROR : "+error.message);
     }
 });
+
+
+userRouter.get("/feed", userAuth, async (req,res)=>{
+    try {
+
+        const page = parseInt(req.query.page) || 1 // if page is not given then assume 1
+        let limit = parseInt(req.query.limit) || 10; // if limit is not given then assume 10
+        limit = limit>50 ? 50 : limit;
+        const skip = (page-1)*limit
+        
+        // user should see all the users card except
+        // his own card
+        // his connections i.e already connections i.e accepted/rejected
+        // ignored people i.e whom the user is ignored to the people means jo ignore kar diye ha
+        // already sent the connection request 
+        // means humko vo cards nahi dikhane ha jisko hum req bhejh chuke ha ya jisne hume req bheji ha ya jjisko humse ignore kar diya ha and apna card
+        const loggedInUser = req.user;
+
+        //find all the connection request (sent+received)
+        const connectionRequests = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id},
+            ],
+        })
+        .select("fromUserId toUserId")
+        // .populate("fromUserId","firstName")
+        // .populate("toUserId","firstName")
+
+        //calculate the user id of the people jinko hume nahi dikhana ha and then push them one by one in the set(hideUserFromFeed)
+        const hideUserFromFeed = new Set();
+        connectionRequests.forEach((req) =>{
+            hideUserFromFeed.add(req.fromUserId.toString()); //push the user jisne bhejhi ha
+            hideUserFromFeed.add(req.toUserId.toString()); // push the user jisko bheji ha
+        })
+
+        // console.log(connectionRequests);
+        // find the remaining users
+        const users = await User.find({
+            $and: [
+                {_id:{$nin : Array.from(hideUserFromFeed)} }, //unki id jo hideUsersFromFeed array me nahi ha i.e $nin and & query is used because both these condtions should be true
+                {_id : {$ne : loggedInUser._id} }, //meri khud ki id bhi nahi honi chaiye i.e not equal (ne)
+            ]
+        })
+        .select(USER_SAFE_DATA) // to display only required fields
+        .skip(skip)
+        .limit(limit)
+
+        res.send(users)
+
+    } catch (error) {
+        res.status(400).send("ERROR : "+error.message);   
+    }
+})
 
 module.exports = userRouter;
